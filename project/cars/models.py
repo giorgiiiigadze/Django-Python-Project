1d2783c66a9a53a997cebc6bf0cca14040e7ae45
giorgi giorgadze <gio.giorgigiorgadze20@gmail.com>
2025-09-09 22:52:14 +0400

    Initialazed project startapp

---
+++ project/cars/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.auth.models import User
+# Create your models here.
+
+class Car(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posted_cars')
+    brand = models.CharField(max_length=50, unique=False)
+    model = models.CharField(max_length=50)
+    year = models.IntegerField(default=2025)
+    daily_price = models.DecimalField(max_digits=10, decimal_places=2, default=1)
+    capacity = models.IntegerField()
+    transmission = models.CharField(max_length=20, choices=[
+        ('automatic', 'Automatic'),
+        ('manual', 'Manual'),
+        ('tiptronic', 'Tiptronic'),
+    ], default='automatic')
+    city = models.CharField(max_length=50, default='kutaisi')
+    fuel_tank = models.IntegerField(default=1)
+    
+    photo1 = models.ImageField(upload_to='cars/', default='cars/default1.jpg')
+    photo2 = models.ImageField(upload_to='cars/', default='cars/default2.jpg')
+    photo3 = models.ImageField(upload_to='cars/', default='cars/default3.jpg')
+    
+    license_plate = models.CharField(max_length=20, unique=True)
+
+    is_available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.brand} {self.model} {self.year}"
+
+class RentCar(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    car = models.ForeignKey(Car, on_delete=models.CASCADE)
+    days = models.IntegerField(default=1)
+    
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f'{self.user} renter the {self.car.brand} {self.car.model}'
---
+++ project/cars/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from .models import *
+# from django.contrib.auth.models import User
+
+
+class CarSerialazer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Car
+        fields = [
+            'id',
+            'owner', 'brand', 'model', 'year', 'capacity', 
+            'transmission', 'daily_price', 'city', 
+            'photo1', 'photo2', 'photo3', 'license_plate' ,'is_available'
+        ]
+        read_only_fields = ['id','owner']
+
+class RentCarSerialazer(serializers.ModelSerializer):
+    class Meta:
+        model = RentCar
+        fields = ["id", "user", "car", "days", "total_price"]
+        read_only_fields = ["user", "total_price"]
